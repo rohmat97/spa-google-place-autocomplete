cf92e2b61398fda8af61b289e261e6998dddf7b0
Rohmat Dasuki <rohmat.dasuki@paynet.my>
2025-11-20 14:33:26 +0800

    chore: remove unused components and standardize code formatting
    - Removed Counter and DataDisplay components from Home page
    - Cleaned up unused imports (Counter, DataDisplay)
    - Standardized quote style from single to double quotes
    - Improved text formatting with multi-line JSX for readability

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Card, Space, Typography, Divider } from 'antd'
-import Counter from '../components/Counter'
-import DataDisplay from '../components/DataDisplay'
+import { Card, Space, Typography, Divider } from "antd";
 import PlaceAutocomplete from '../components/PlaceAutocomplete'
 import PlaceMap from '../components/PlaceMap'
 
@@ -11,28 +9,23 @@ const Home = () => {
   return (
     <div>
       <Title level={2}>Place Search</Title>
-      <Space direction="vertical" style={{ width: '100%' }}>
+      <Space direction="vertical" style={{ width: "100%" }}>
         <Card title="Search for Places">
-          <p>Use the search box below to find places using Google Place Autocomplete.</p>
+          <p>
+            Use the search box below to find places using Google Place
+            Autocomplete.
+          </p>
           <PlaceAutocomplete />
         </Card>
-        
+
         <Card title="Place Locations">
           <PlaceMap />
         </Card>
-        
+
         <Divider />
-        
-        <Card title="Redux Counter">
-          <Counter title="Page-level Counter" />
-        </Card>
-        
-        <Card title="Data Display">
-          <DataDisplay />
-        </Card>
       </Space>
     </div>
-  )
+  );
 }
 
 export default Home
